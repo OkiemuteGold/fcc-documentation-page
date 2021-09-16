6aecbdd3c33632cb0837d0fd71721a12529bf557
Okiemute Gold <goldokiemute1@gmail.com>
2021-09-16 17:23:50 +0100

    changed scrollbar design

--- js/main.js
+++ js/main.js
@@ -25,12 +25,9 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 });
 
 /** update footer year **/
-let footerYear = document.getElementById("footer-year");
-let fullCopyrightYear;
-let date = new Date();
-let currentYear = date.getFullYear();
-currentYear > 2021 ? fullCopyrightYear = `2021 - ${currentYear}` : fullCopyrightYear = currentYear;
-footerYear.innerHTML = fullCopyrightYear;
-
-let navbar = document.getElementById("nav-bar")
-let toggler = document.querySelector("#mobile-menu-toggler");
+// let footerYear = document.getElementById("footer-year");
+// let fullCopyrightYear;
+// let date = new Date();
+// let currentYear = date.getFullYear();
+// currentYear > 2021 ? fullCopyrightYear = `2021 - ${currentYear}` : fullCopyrightYear = currentYear;
+// footerYear.innerHTML = fullCopyrightYear;
