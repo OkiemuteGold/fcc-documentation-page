d8a0aa39f01166121ca55750cb2e02fdd0e97564
Okiemute Gold <goldokiemute1@gmail.com>
2021-09-26 02:44:35 +0100

    finished layout, added all navs

--- js/main.js
+++ js/main.js
@@ -25,9 +25,9 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 });
 
 /** update footer year **/
-// let footerYear = document.getElementById("footer-year");
-// let fullCopyrightYear;
-// let date = new Date();
-// let currentYear = date.getFullYear();
-// currentYear > 2021 ? fullCopyrightYear = `2021 - ${currentYear}` : fullCopyrightYear = currentYear;
-// footerYear.innerHTML = fullCopyrightYear;
+let footerYear = document.getElementById("footer-year");
+let fullCopyrightYear;
+let date = new Date();
+let currentYear = date.getFullYear();
+currentYear > 2021 ? fullCopyrightYear = `2021 - ${currentYear}` : fullCopyrightYear = currentYear;
+footerYear.innerHTML = fullCopyrightYear;
