f8985e051d74e8bcb6141f3ddfda0cba86ca31ae
Okiemute Gold <goldokiemute1@gmail.com>
2021-10-02 05:35:34 +0100

    added night theme, modified css, changed tooltip for themes page scrollbar

--- js/main.js
+++ js/main.js
@@ -31,3 +31,70 @@ let date = new Date();
 let currentYear = date.getFullYear();
 currentYear > 2021 ? fullCopyrightYear = `2021 - ${currentYear}` : fullCopyrightYear = currentYear;
 footerYear.innerHTML = fullCopyrightYear;
+
+/** mode toggle - day/night **/
+const body = document.body;
+let modeIcon = document.querySelector(".mode-icon");
+
+modeIcon.addEventListener("click", function (e) {
+    // this.classList.toggle("fa-sun");
+
+    this.classList.contains("fa-moon") ?
+        (this.classList.replace("fa-moon", "fa-sun"),
+            this.setAttribute("aria-label", "use day mode")) :
+        ((this.className = "fas fa-moon mode-icon"),
+            this.setAttribute("aria-label", "use night mode"));
+
+    /** change theme **/
+    if (body.classList.contains("nightTheme")) {
+        body.classList.replace("nightTheme", "dayTheme");
+
+        currentTheme = JSON.stringify("dayTheme");
+        localStorage.setItem("theme", currentTheme);
+    } else {
+        body.className = "nightTheme";
+
+        currentTheme = JSON.stringify("nightTheme");
+        localStorage.setItem("theme", currentTheme);
+    }
+});
+
+/** get and save current theme **/
+getCurrentTheme();
+
+function getCurrentTheme() {
+
+    window.onload = setDefault();
+
+    let currentTheme = JSON.parse(localStorage.getItem("theme")) ? JSON.parse(localStorage.getItem("theme")) : null;
+
+    const body = document.body;
+    const modeIcon = document.querySelector(".mode-icon");
+
+    if (currentTheme !== null) {
+        if (currentTheme == 'nightTheme') {
+            body.className = "nightTheme";
+
+            currentTheme = JSON.stringify("nightTheme");
+            localStorage.setItem("theme", currentTheme);
+
+            modeIcon.classList.replace("fa-moon", "fa-sun");
+            modeIcon.setAttribute("aria-label", "use day mode");
+
+        } else if (currentTheme == "dayTheme") {
+            body.className = "dayTheme";
+
+            currentTheme = JSON.stringify("dayTheme");
+            localStorage.setItem("theme", currentTheme);
+
+            modeIcon.className = "fas fa-moon mode-icon";
+            modeIcon.setAttribute("aria-label", "use night mode");
+        }
+    }
+}
+
+/** set default theme on load **/
+function setDefault() {
+    body.className = "dayTheme";
+    // console.log("loaded");
+}
